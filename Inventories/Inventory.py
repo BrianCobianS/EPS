d631d91f1d55f3635406e893305a44c085380cdd
brian.cobian <brian.cobian@toshibagcs.com>
2022-12-02 17:19:25 -0600

    DELETING RPML FOR ACE4D

--- Inventories/Inventory.py
+++ Inventories/Inventory.py
@@ -10,4 +10,4 @@ def controller(host,user,password):
     f.writelines("\n".join(contenido))
 controller('10.89.105.98','master','m1')
 
-# ansible-playbook playbooks/os4690/Install_Controller.yml -vv  -i Inventories/import_inventory.yml -e 'level_complement=V8R1SP2.j292.1-20221111.223820-1 opc=1 ASM=Accept level_name=V8R1SP2.j292 level_complementEPS=V8R1SP2.j292.1-20221111.223829-1' 2>&1 | tee temp.txt
+# ansible-playbook playbooks/os4690/Install_Controller.yml -vv  -i Inventories/import_inventory.yml -e 'level_complement=V8R1SP2.j292.1-20221111.223820-1 opc=1 ASM=Accept level_name=V8R1SP2.j292.1 level_complementEPS=V8R1SP2.j292.1-20221111.223829-1' 2>&1 | tee temp.txt
