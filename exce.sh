7d43b221b3d5f5fbc025bda1f3626280f56bf36c
brian.cobian <brian.cobian@toshibagcs.com>
2022-12-03 11:52:50 -0600

    EPS TESR

--- exce.sh
+++ exce.sh
@@ -1,1 +1,1 @@
-ansible-playbook playbooks/os4690/Install_Controller.yml -vv  -i Inventories/import_inventory.yml -e 'level_complement=V8R1SP2.j301-20221110.173537-1 opc=1 ASM=Accept level_name=V8R1SP2.j301 level_complementEPS=V8R1SP2.j301-20221110.173550-1' 2>&1 | tee temp.txt
+ansible-playbook playbooks/os4690/Install_Controller.yml -vv  -i Inventories/import_inventory.yml -e 'level_complement=V8R1SP2.j301-20221110.173537-1 opc=2 ASM=Test level_name=V8R1SP2.j301 level_complementEPS=V8R1SP2.j301-20221110.173550-1' 2>&1 | tee temp.txt
