b242ee20cfbc1c4a0189821f4901425a1914dde7
brian.cobian <brian.cobian@toshibagcs.com>
2022-12-05 10:45:01 -0600

    Changing AUTO.BAT and PRINT opc TASK also Upfile.py

--- exce.sh
+++ exce.sh
@@ -1,1 +1,1 @@
-ansible-playbook playbooks/os4690/Install_Controller.yml -vv  -i Inventories/import_inventory.yml -e 'level_complement=V8R1SP2.j301-20221110.173537-1 opc=2 ASM=Accept level_name=V8R1SP2.j301 level_complementEPS=V8R1SP2.j301-20221110.173550-1' 2>&1 | tee temp.txt
+ansible-playbook playbooks/os4690/Install_Controller.yml -vv  -i Inventories/import_inventory.yml -e 'level_complement=V8R1SP2.j302-20221117.173637-1 opc=2 ASM=Accept level_name=V8R1SP2.j302 level_complementEPS=V8R1SP2.j302-20221117.173651-1' 2>&1 | tee temp.txt
