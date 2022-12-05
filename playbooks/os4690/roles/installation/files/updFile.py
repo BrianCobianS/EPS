2f7e44d7dcdd51e1c8fe2111d1e4e6412565bd7f
brian.cobian <brian.cobian@toshibagcs.com>
2022-12-05 12:41:30 -0600

    Changing AUTO.BAT and PRINT opc TASK also Upfile.py

--- playbooks/os4690/roles/installation/files/updFile.py
+++ playbooks/os4690/roles/installation/files/updFile.py
@@ -1,3 +1,4 @@
+import sys
 def changeBat(fileDirectory, wordToFind, wordToChange, incremento):
     with open(fileDirectory,'r') as archivo:
         i=0
@@ -68,29 +69,40 @@ def erasepauses(archivo):
     f.writelines("\n".join(contenido))
     f.close
 
-
-changeBat('/mnt/c/ace3d/install.bat','MENU\n','AUTO3D.BAT', 0)
-changeBat('/mnt/c/ace3d/1.bat','MENU\n','CLS\n9.BAT', 2)
-changeBat('/mnt/c/ace3d/1.bat','menu\n','CLS\n9.BAT', 0)
-changeBat('/mnt/c/ace3d/2.bat','   menu\n','   CLS\n   9.BAT', 2)
-changeBat('/mnt/c/ace3d/2.bat','menu\n','CLS\n9.BAT', 2)
-
-erasepauses("/mnt/c/ace3d/1.bat")
-erasepauses("/mnt/c/ace3d/2.bat")
-erasepauses("/mnt/c/ace3d/0.bat")
-erasepauses("/mnt/c/ace3d/7.bat")
-erasepauses("/mnt/c/ace3d/8.bat")
-erasepauses("/mnt/c/ace3d/EAMRMCDZ.bat")
-erasepauses("/mnt/c/ace3d/PPINSWIN.bat")
-
-changeBat('/mnt/c/ace4d/install.bat','MENU\n','AUTO4D.BAT', 0)
-changeBat('/mnt/c/ace4d/1.bat','MENU\n','CLS\n9.BAT', 2)
-changeBat('/mnt/c/ace4d/1.bat','menu\n','CLS\n9.BAT', 0)
-changeBat('/mnt/c/ace4d/2.bat','   menu\n','   CLS\n   9.BAT', 2)
-changeBat('/mnt/c/ace4d/2.bat','menu\n','CLS\n9.BAT', 2)
-erasepauses("/mnt/c/ace4d/1.bat")
-erasepauses("/mnt/c/ace4d/2.bat")
-erasepauses("/mnt/c/ace4d/0.bat")
-erasepauses("/mnt/c/ace4d/7.bat")
-erasepauses("/mnt/c/ace4d/8.bat")
-erasepauses("/mnt/c/ace4d/MIGRATE.bat")
+if sys.argv[1]==3:
+    changeBat('/mnt/c/ace3d/install.bat','MENU\n','AUTO3D.BAT', 0)
+    changeBat('/mnt/c/ace3d/1.bat','MENU\n','CLS\n9.BAT', 2)
+    changeBat('/mnt/c/ace3d/1.bat','menu\n','CLS\n9.BAT', 0)
+    changeBat('/mnt/c/ace3d/2.bat','   menu\n','   CLS\n   9.BAT', 2)
+    changeBat('/mnt/c/ace3d/2.bat','menu\n','CLS\n9.BAT', 2)
+    erasepauses("/mnt/c/ace3d/1.bat")
+    erasepauses("/mnt/c/ace3d/2.bat")
+    erasepauses("/mnt/c/ace3d/0.bat")
+    erasepauses("/mnt/c/ace3d/7.bat")
+    erasepauses("/mnt/c/ace3d/8.bat")
+    erasepauses("/mnt/c/ace3d/EAMRMCDZ.bat")
+    erasepauses("/mnt/c/ace3d/PPINSWIN.bat")
+elif sys.argv[1]==4:
+    changeBat('/mnt/c/ace4d/install.bat','MENU\n','AUTO4D.BAT', 0)
+    changeBat('/mnt/c/ace4d/1.bat','MENU\n','CLS\n9.BAT', 2)
+    changeBat('/mnt/c/ace4d/1.bat','menu\n','CLS\n9.BAT', 0)
+    changeBat('/mnt/c/ace4d/2.bat','   menu\n','   CLS\n   9.BAT', 2)
+    changeBat('/mnt/c/ace4d/2.bat','menu\n','CLS\n9.BAT', 2)
+    erasepauses("/mnt/c/ace4d/1.bat")
+    erasepauses("/mnt/c/ace4d/2.bat")
+    erasepauses("/mnt/c/ace4d/0.bat")
+    erasepauses("/mnt/c/ace4d/7.bat")
+    erasepauses("/mnt/c/ace4d/8.bat")
+    erasepauses("/mnt/c/ace4d/MIGRATE.bat")
+elif sys.argv[1]==6:
+    changeBat('/mnt/c/ace6d/install.bat','MENU\n','AUTO4D.BAT', 0)
+    changeBat('/mnt/c/ace6d/1.bat','MENU\n','CLS\n9.BAT', 2)
+    changeBat('/mnt/c/ace6d/1.bat','menu\n','CLS\n9.BAT', 0)
+    changeBat('/mnt/c/ace6d/2.bat','   menu\n','   CLS\n   9.BAT', 2)
+    changeBat('/mnt/c/ace6d/2.bat','menu\n','CLS\n9.BAT', 2)
+    erasepauses("/mnt/c/ace6d/1.bat")
+    erasepauses("/mnt/c/ace6d/2.bat")
+    erasepauses("/mnt/c/ace6d/0.bat")
+    erasepauses("/mnt/c/ace6d/7.bat")
+    erasepauses("/mnt/c/ace6d/MIGRATE.bat")
+    erasepauses("/mnt/c/ace6d//EAMRMCDZ.bat")
